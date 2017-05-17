2925eceeb59a1b76e75634b48375be1069204c48
ningxiaoxiao <732869825@qq.com>
2017-05-17 16:54:01 +0800

    update openshift arg

--- routes/book.js
+++ routes/book.js
@@ -2,16 +2,38 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize('yjbook', 'root', '123456', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 mongoose.Promise = global.Promise;
-var db = mongoose.createConnection('mongodb://localhost/yjbook');
+
+
+var mongoUrl = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+    mongoURLLabel = "";
+
+if (mongoURL === null && process.env.DATABASE_SERVICE_NAME) {
+    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+        mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+        mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+        mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+        mongoPassword = process.env[mongoServiceName + '_PASSWORD'],
+        mongoUser = process.env[mongoServiceName + '_USER'];
+
+    if (mongoHost && mongoPort && mongoDatabase) {
+        mongoURLLabel = mongoURL = 'mongodb://';
+        if (mongoUser && mongoPassword) {
+            mongoURL += mongoUser + ':' + mongoPassword + '@';
+        }
+        // Provide UI label that excludes user id and pw
+        mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+        mongoURL += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+
+    }
+
+}
+
+var db = mongoose.createConnection(mongoUrl);
 
 db.on('error', function(err) {
     console.log(err);
